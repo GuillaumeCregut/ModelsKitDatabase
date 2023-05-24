b2a714d2e3624077d3ee8848199ae7ebf8d9a4ce
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-05-24 19:51:52 +0200

    refactor(left menus): change last left menus

--- frontend/src/components/admin/adminmenu/AdminMenu.jsx
+++ frontend/src/components/admin/adminmenu/AdminMenu.jsx
@@ -1,11 +1,9 @@
 import { NavLink } from 'react-router-dom';
 
-import './AdminMenu.scss';
-
 const AdminMenu = () => {
     return (
-        <nav className='admin-menu'>
-            <ul className="admin-links-container">
+        <nav className='left-menu'>
+            <ul className="left-links-container">
                 <li className="link-param-item">
                     <NavLink to="logs" className={({ isActive }) => isActive ? 'nav-item active' : 'nav-item'}>Logs serveur</NavLink>
                 </li>
