cae3168461d8f803c9043506dacb269d48bb36a4
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-06-09 20:24:29 +0200

    refactor(models): change back to add model like if so
    return liked value if so when getting models

--- backend/middlewares/UserValidation.js
+++ backend/middlewares/UserValidation.js
@@ -1,45 +1,66 @@
-const {verifyToken}=require('../utils/auth');
-const {logError}=require('../utils/logEvent');
-const {userRole}=require('../utils/common');
+const { verifyToken } = require('../utils/auth');
+const { logError } = require('../utils/logEvent');
+const { userRole } = require('../utils/common');
 
-const userCheck=(req,res,next)=>{
-   // For later.
+const getidFromToken = (req, res, next) => {
+    const authHeader = req.headers.authorization || req.headers.Authorization;
+    if (authHeader) {
+        try {
+            const token = authHeader.split(' ')[1];
+            const isOK = verifyToken(token, 'access');
+            if (!isOK)
+                return res.sendStatus(403);
+            req.user = isOK;
+        }
+        catch (err) {
+            logError(`UserValidation : ${err}`);
+            console.error(err);
+            return res.sendStatus(500);
+        }
+    }
+    else req.user={user_id:0}
+    next();
+}
+
+const userCheck = (req, res, next) => {
+    // For later.
     // const authHeader=req.headers['authorization'];
     // to be relaced by
-    const authHeader=req.headers.authorization || req.headers.Authorization;
-    if(!authHeader){ 
+    const authHeader = req.headers.authorization || req.headers.Authorization;
+    if (!authHeader) {
         return res.sendStatus(401);
     }
-    try{
-        const token=authHeader.split(' ')[1];
-        const isOK=verifyToken(token,'access');
-        if(!isOK)
+    try {
+        const token = authHeader.split(' ')[1];
+        const isOK = verifyToken(token, 'access');
+        if (!isOK)
             return res.sendStatus(403);
-        req.user=isOK;
+        req.user = isOK;
         next();
     }
-    catch(err){
+    catch (err) {
         logError(`UserValidation : ${err}`);
         console.error(err);
         return res.sendStatus(500);
     }
 }
 
-const checkLevel=(req,res,next)=>{
-    const rankUser=req.user.rank;
-    if (rankUser!==userRole.admin)
+const checkLevel = (req, res, next) => {
+    const rankUser = req.user.rank;
+    if (rankUser !== userRole.admin)
         return res.sendStatus(403);
     next();
 }
 
-const idChecker=(req,res,next)=>{
-    if(isNaN(req.params.id))
+const idChecker = (req, res, next) => {
+    if (isNaN(req.params.id))
         return res.sendStatus(422);
     next();
 }
 
-module.exports={
+module.exports = {
     userCheck,
     checkLevel,
     idChecker,
+    getidFromToken
 }
