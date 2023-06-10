1cb03d9f938f2d0230aefa3c28f5a97ee7312f4e
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-06-10 19:07:54 +0200

    refactor(simple component): replace simple component with table, update OK

---
+++ frontend/src/components/parameters/areacontainer/AreaContainer2.jsx
@@ -0,0 +1,80 @@
+import axios from 'axios';
+import useAxiosPrivate from "../../../hooks/useAxiosPrivate";
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { SimpleCardContainer } from '../simplecardcontainer/SimpleCardContainer';
+import { addPeriod, setPeriod, updatePeriod, deletePeriod } from '../../../feature/Period.slice';
+import { AwaitLoad } from '../../awaitload/AwaitLoad';
+import FormAddSimple from '../formaddsimple/FormAddSimple';
+import { ToastContainer, toast } from 'react-toastify';
+
+import SimpleArray from '../simplecardcontainer/SimpleArray';
+
+const AreaContainer2 = () => {
+    const [isLoaded, setIsLoaded] = useState(false);
+    const dispatch = useDispatch();
+    const periodsData = useSelector((state) => state.periods.period);
+    const url = `${import.meta.env.VITE_APP_API_URL}period`;
+    const axiosPrivate = useAxiosPrivate();
+
+    const columns = [
+        { field: 'name', headerName: 'Nom', width: 130 },
+        { field: 'id', headerName: 'Modifier', width: 130 },
+        { field: 'id', headerName: 'Supprimer', width: 130 },
+    ]
+
+    const addAction = (newData) => {
+        if (window.confirm("Voulez vous ajouter l'élément ?")) {
+            axiosPrivate
+                .post(url, newData)
+                .then((resp) => {
+                    const newPeriod = resp.data;
+                    dispatch(addPeriod(newPeriod));
+                })
+                .catch((err) => {
+                    toast.error("Vous n'êtes pas autorisé à ajouter un élément.")
+                })
+        }
+    }
+
+    const getPeriods = () => {
+        axios.get(url)
+            .then((res) => {
+                dispatch(setPeriod(res.data))
+                setIsLoaded(true);
+            });
+    }
+
+    useEffect(() => {
+        getPeriods();
+    }, []);
+
+    //Bundling datas for card
+    const wrapper = {
+        deleteAction: deletePeriod,
+        updateAction: updatePeriod,
+        kind: "la période",
+        url: url
+    }
+
+
+    return (
+        <section className="right-page">
+            <ToastContainer />
+            <h2 className='solo-title'>Les périodes</h2>
+            <div className='solo-container'>
+                {isLoaded
+                    ? <SimpleArray item={periodsData}
+                        wrapper={wrapper} />
+
+                    : <AwaitLoad />
+                }
+            </div>
+            <FormAddSimple
+                action={addAction}
+            />
+        </section>
+    )
+}
+
+export default AreaContainer2
---
+++ frontend/src/components/parameters/simplecardcontainer/SimpleArray.jsx
@@ -0,0 +1,112 @@
+import { DataGrid } from '@mui/x-data-grid';
+import { useCallback, useEffect, useState } from 'react';
+import ranks from '../../../feature/ranks';
+import useAuth from '../../../hooks/useAuth';
+import { ToastContainer, toast } from 'react-toastify';
+import useAxiosPrivate from '../../../hooks/useAxiosPrivate';
+import { useDispatch } from 'react-redux';
+
+const SimpleArray = ({ item, wrapper }) => {
+    const axiosPrivate = useAxiosPrivate();
+    const dispatch=useDispatch();
+    const { deleteAction, updateAction, kind, url } = wrapper;
+    const { auth } = useAuth();
+    let rankUser = auth?.rank;
+    if (!rankUser)
+        rankUser = 0;
+    let oldName = '';
+
+    const columns = [
+        {
+            field: 'name',
+            headerName: 'Nom',
+            width: 130,
+            editable: rankUser === ranks.admin,
+        },
+        {
+            field: 'id',
+            headerName: 'Action',
+            width: 130,
+            renderCell: (params) => {
+                return (rankUser === ranks.admin ? <button onClick={() => handleDelete(params.value)}>Supprimer </button> : null);
+            },
+        },
+    ]
+
+    const handleDelete = (id) => {
+        alert(`Voulez vous supprimer ${id} ?`)
+    }
+
+    const updateData=async(newRow,old)=>{
+        const newItem = { name: newRow.name };
+        const urlApi = `${url}/${newRow.id}`;
+       return axiosPrivate
+            .put(urlApi, newItem)
+            .then(() => {
+                dispatch(updateAction([newItem, newRow.id]))
+                return({...newRow, isNew: false })
+            })
+            .catch((err) => {
+                switch (err?.response?.status) {
+                    case 401: toast.error("Vous n'êtes pas autoriser à modifier");
+                        break;
+                    case 403: toast.error("Vous n'êtes pas autoriser à modifier");
+                        break;
+                    case 404: toast.warn("L'élément n'existe pas");
+                        break;
+                    case 422: toast.warn("Veuillez vérifier les valeurs");
+                        break;
+                    case 500: toast.error("Une erreur serveur est survenue.");
+                        break;
+                    default: toast.error(`action impossible : ${err?.response?.status}`);
+                }
+                newRow.name=old;
+                return {...newRow, isNew: false };   
+            })
+            
+    }
+
+    const handleStartEdit = useCallback((params) => {
+        oldName = params.value;
+    });
+
+    const handleProcess = useCallback(
+        async (newRow) => {
+            const updatedRow = { ...newRow, isNew: false };
+            if (window.confirm(`Voulez vous modifier ${newRow.id} avec ${newRow.name}?`)) {
+             return  await updateData(updatedRow,oldName);
+            }
+            else {           
+                updatedRow.name = oldName;
+                return updatedRow;
+            }
+        }, []);
+
+    const handleProcessRowUpdateError = useCallback((error) => {
+        console.log(error.message);
+    }, []);
+
+    return (
+        <>
+            <ToastContainer />
+            <DataGrid
+                //  onCellDoubleClick={handleEvent}
+                isRowSelectable={() => false}
+                onCellEditStart={handleStartEdit}
+                processRowUpdate={handleProcess}
+                onProcessRowUpdateError={handleProcessRowUpdateError}
+                rows={item}
+                columns={columns}
+                initialState={{
+                    pagination: {
+                        paginationModel: { page: 0, pageSize: 5 },
+                    },
+                }}
+                pageSizeOptions={[5, 10]}
+                checkboxSelection
+            />
+        </>
+    )
+}
+
+export default SimpleArray
