0fa561a6306035d55aaa10fb91ecaad70a53329c
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-06-16 19:51:16 +0200

    refactor(orders): changing display for orders

--- frontend/src/components/userprofil/orders/ModelLine.jsx
+++ frontend/src/components/userprofil/orders/ModelLine.jsx
@@ -1,4 +1,6 @@
 import { AiFillMinusCircle, AiFillPlusSquare } from "react-icons/ai";
+import TableCell from '@mui/material/TableCell';
+import TableRow from '@mui/material/TableRow';
 
 import './ModelLine.scss';
 
@@ -11,17 +13,17 @@ const ModelLine = ({ model, setNewQtty }) => {
     }
 
     return (
-        <tr className="line-model-order">
-            <td className="cell-model-order">{model.builder} {model.name}</td>
-            <td className="cell-model-order">{model.brand}</td>
-            <td className="cell-model-order">{model.scale}</td>
-            <td className="cell-model-order">
+        <TableRow className="line-model-order">
+            <TableCell className="cell-model-order">{model.builder} {model.name}</TableCell>
+            <TableCell className="cell-model-order">{model.brand}</TableCell>
+            <TableCell className="cell-model-order">{model.scale}</TableCell>
+            <TableCell className="cell-model-order">
                 <AiFillMinusCircle className="line-icon remove-qtty" onClick={()=>handleClick(-1)}/>
                 {model.qtty}
                 <AiFillPlusSquare className="line-icon add-qtty" onClick={()=>handleClick(1)}/>
-            </td>
-            <td className="cell-model-order">{model.price} </td>
-        </tr>
+            </TableCell>
+            <TableCell className="cell-model-order">{model.price} euros</TableCell>
+        </TableRow>
     )
 }
 
