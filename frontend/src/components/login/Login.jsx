eac8677301c6cc85c7a9f16421302ccde7edf32c
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-06-25 17:45:41 +0200

    refactor(login page): change input style

--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -4,16 +4,20 @@ import { Link, useNavigate } from 'react-router-dom';
 import useAuth from '../../hooks/useAuth';
 import jwt_decode from "jwt-decode";
 import { ToastContainer, toast } from 'react-toastify';
+import { IconButton } from '@mui/material';
+import {MdVisibilityOff,MdVisibility} from "react-icons/md";
+
 import './Login.scss';
 
 const Login = () => {
     const [login, setLogin] = useState('');
     const [pass, setPass] = useState('');
+    const [showPassword,setShowPassword]=useState(false);
     const [errMsg, setErrMsg] = useState(false);
     const { setAuth } = useAuth();
     const loginRef = useRef();
     const errRef = useRef();
-    const navigate=useNavigate();
+    const navigate = useNavigate();
     useEffect(() => {
         loginRef.current.focus();
     }, [])
@@ -41,16 +45,16 @@ const Login = () => {
                         const user = {
                             firstname: decoded.firstname,
                             lastname: decoded.lastname,
-                            id:decoded.user_id,
+                            id: decoded.user_id,
                             rank: decoded.rank,
-                            token: {accessToken : token}
+                            token: { accessToken: token }
                         }
                         setAuth(user);
-                        const userToStorage={
-                            firstname:decoded.firstname,
-                            lastname:decoded.lastname
+                        const userToStorage = {
+                            firstname: decoded.firstname,
+                            lastname: decoded.lastname
                         }
-                        localStorage.setItem('ModelsKitUser',JSON.stringify(userToStorage))
+                        localStorage.setItem('ModelsKitUser', JSON.stringify(userToStorage))
                         navigate('/');
                     }
                 })
@@ -75,10 +79,11 @@ const Login = () => {
             <h2 className="login-title">Connexion</h2>
             <p className={errMsg ? "login-error-msg" : "login-hide"} aria-live="assertive" ref={errRef}>{errMsg} </p>
             <form className='login-form' onSubmit={handleSubmit}>
-                <label htmlFor="login" className="login-label">Login :
+                <label htmlFor="login" className="login-label">
                     <input
                         type="text"
                         className="input-login-form"
+                        placeholder='login'
                         id="login"
                         required
                         autoComplete='off'
@@ -87,15 +92,19 @@ const Login = () => {
                         onChange={(e) => setLogin(e.target.value)}
                     />
                 </label>
-                <label htmlFor="password" className="login-label">Mot de passe :
-                    <input
-                        type="password"
-                        className="input-login-form"
-                        id="password"
-                        required
-                        value={pass}
-                        onChange={(e) => setPass(e.target.value)}
-                    />
+                <label htmlFor="password" className="login-label">
+                    <span className="login-pwd">
+                        <input
+                            type={showPassword?"text":"password"}
+                            className="input-login-form"
+                            placeholder='Mot de passe'
+                            id="password"
+                            required
+                            value={pass}
+                            onChange={(e) => setPass(e.target.value)}
+                        />
+                        <IconButton onClick={()=>{setShowPassword(!showPassword)}} > {!showPassword?<MdVisibility />:<MdVisibilityOff />} </IconButton>
+                    </span>
                 </label>
                 <button className='login-btn'>Connexion</button>
             </form>
