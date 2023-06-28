a37ee917bc3000a8a69fc0106d8385eb8f94a099
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-06-28 16:12:30 +0200

    refactor(kit mgmt): change modal

--- frontend/src/components/kits/kitmgmt/kitcard/KitCard.jsx
+++ frontend/src/components/kits/kitmgmt/kitcard/KitCard.jsx
@@ -1,36 +1,55 @@
 
+import { useState } from 'react';
 import useAxiosPrivate from '../../../../hooks/useAxiosPrivate';
 import { FaRegTrashAlt } from "react-icons/fa";
 import { toast } from 'react-toastify';
 import { useDispatch } from "react-redux";
 import { deleteStock } from "../../../../feature/stockUser.slice";
+import { Dialog, DialogActions, DialogContent, DialogContentText, IconButton } from '@mui/material';
 
 import './KitCard.scss';
 
-const KitCard = ({ kitDetails, refresh=false, setRefresh=null, displayImage = false, inMgmt = false }) => {
+const KitCard = ({ kitDetails, refresh = false, setRefresh = null, displayImage = false, inMgmt = false }) => {
+    const [openModal, setOpenModal] = useState(false);
     const url = `${import.meta.env.VITE_APP_URL}`;
     const axiosPrivate = useAxiosPrivate();
     const dispatch = useDispatch();
 
-    const deleteKit = () => {
-        if (window.confirm('Voulez-vous supprimer ce kit ?')) {
-            const urlStock = `${import.meta.env.VITE_APP_API_URL}users/model/${kitDetails.id}`;
-            axiosPrivate
-                .delete(urlStock)
-                .then(() => {
-                    dispatch(deleteStock(kitDetails.id));
-                    setRefresh(!refresh);
-                })
-                .catch((err) => {
-                    console.log(err);
-                    toast.error('Une erreur est survenue');
-                })
+    const handleClose = () => {
+        setOpenModal(false);
+    }
 
-        }
+    const deleteKit = () => {
+        const urlStock = `${import.meta.env.VITE_APP_API_URL}users/model/${kitDetails.id}`;
+        axiosPrivate
+            .delete(urlStock)
+            .then(() => {
+                dispatch(deleteStock(kitDetails.id));
+                setRefresh(!refresh);
+            })
+            .catch((err) => {
+                console.log(err);
+                toast.error('Une erreur est survenue');
+            })
     }
 
     return (
         <div className='kit-card'>
+            <Dialog
+                open={openModal}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description">
+                <DialogContent>
+                    <DialogContentText>
+                        Voulez-vous supprimer ce kit ?
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <IconButton onClick={handleClose}>Non</IconButton>
+                    <IconButton onClick={deleteKit}>Oui</IconButton>
+                </DialogActions>
+            </Dialog>
             <h4>{kitDetails.modelName} - {kitDetails.builderName}</h4>
             {displayImage && <img src={`${url}${kitDetails.boxPicture}`} alt={kitDetails.modelName} />}
             <p>{kitDetails.brandName} - {kitDetails.scaleName} </p>
@@ -38,7 +57,7 @@ const KitCard = ({ kitDetails, refresh=false, setRefresh=null, displayImage = fa
             <div className="delete-btn-container">
                 {inMgmt
                     ? null
-                    : <button className='btn' onClick={deleteKit}><FaRegTrashAlt className='icon' /></button>
+                    : <button className='btn' onClick={() => setOpenModal(true)}><FaRegTrashAlt className='icon' /></button>
                 }
             </div>
         </div>
