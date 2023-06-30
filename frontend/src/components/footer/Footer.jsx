675de97016bc6b0ae0cfcd7a31816975815300dd
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-06-30 13:09:49 +0200

    style(rename version): rename version in footer

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ import logo from '../../assets/pictures/logo-mini.gif';
 export const Footer = () => {
   return (
     <footer className="footer-page">
-       <img src={logo} alt="Logo" /> <span className='copyright'>(c) 2023 Editiel 98 v 1.1sa</span>
+       <img src={logo} alt="Logo" /> <span className='copyright'>(c) 2023 Editiel 98 v 1.1a</span>
     </footer>
   )
 }
