d9a87f2631ff1cb5ae3e9ba297f6e3420cfec3d1
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-07-02 22:39:42 +0200

    refactor(user): CRUD user OK

--- backend/models/users.model.js
+++ backend/models/users.model.js
@@ -1,149 +1,140 @@
 const { dbquery } = require('../utils/dbutils');
-const User=require('../classes/User.class');
+const User = require('../classes/User.class');
 
-const findAll=async()=>{
+const findAll = async () => {
     const dbResult = await dbquery('get', 'SELECT firstname,id,lastname,rankUser,login, email FROM user');
-    if (dbResult && dbResult !== -1) {
-        const resultat = dbResult.map(element => {
-            const item = new User(element.firstname,element.lastname,element.login,null,element.rankUser,element.email,element.id);
+    if (dbResult.error === 0) {
+        const resultat = dbResult.result.map(element => {
+            const item = new User(element.firstname, element.lastname, element.login, null, element.rankUser, element.email, element.id);
             return item;
         });
         return resultat;
     }
-    else if(dbResult===-1)
-    {
-        return undefined;
-    }
     else
-        return -1;
+        return dbResult.result;
 
 }
 
-const findOne=async(id)=>{
-    const dbResult = await dbquery('get', 'SELECT firstname,id,lastname,rankUser,login, email FROM user WHERE id=?',[id]);
-    if (dbResult && dbResult !== -1) {
-        const resultat = dbResult.map(element => {
-            const item = new User(element.firstname,element.lastname,element.login,null,element.rankUser,element.email,element.id);
+const findOne = async (id) => {
+    const dbResult = await dbquery('get', 'SELECT firstname,id,lastname,rankUser,login, email FROM user WHERE id=?', [id]);
+    if (dbResult.error === 0) {
+        const dbArray = dbResult.result;
+        const resultat = dbArray.map(element => {
+            const item = new User(element.firstname, element.lastname, element.login, null, element.rankUser, element.email, element.id);
             return item;
         });
         return resultat;
     }
-    else if(dbResult===-1)
-    {
-        return undefined;
-    }
     else
-        return -1;
-
+        return dbResult.result
 }
 
-const findOneByLogin=async (login)=>{
-    const dbResult = await dbquery('get', 'SELECT firstname,id,lastname,rankUser,passwd FROM user WHERE login=?',[login]);
-    if (dbResult && dbResult !== -1) {
-        const resultat = dbResult.map(element => {
-            const item = new User(element.firstname,element.lastname,login,element.passwd,element.rankUser,null,element.id);
+const findOneByLogin = async (login) => {
+    const dbResult = await dbquery('get', 'SELECT firstname,id,lastname,rankUser,passwd FROM user WHERE login=?', [login]);
+    if (dbResult.error === 0) {
+        const dbArray = dbResult.result;
+        const resultat = dbArray.map(element => {
+            const item = new User(element.firstname, element.lastname, login, element.passwd, element.rankUser, null, element.id);
             return item;
         });
         return resultat;
     }
-    else if(dbResult===-1)
-    {
-        return undefined;
-    }
     else
-        return -1;
+        return dbResult.result;
 
 }
 
 
-const addUser=async(user)=>{
-    
-    const paramsArray=Object.values(user);
-    const [id,...params]=paramsArray;
-    const checkUnique=await dbquery('get','SELECT count(*) as nb FROM `user` WHERE login=? OR email=?',[user.login,user.email]);
-    const nbUser=checkUnique[0].nb;
-    if(nbUser>0){
-        return -2;
+const addUser = async (user) => {
+
+    const paramsArray = Object.values(user);
+    const [id, ...params] = paramsArray;
+    const checkUnique = await dbquery('get', 'SELECT count(*) as nb FROM `user` WHERE login=? OR email=?', [user.login, user.email]);
+    if (checkUnique.error === 0) {
+        const nbUser = checkUnique.result[0].nb;
+        if (nbUser > 0) {
+            return -2;
+        }
     }
-    const dbResult= await dbquery('add','INSERT INTO user (firstname, lastname, login,passwd,rankUser, email) VALUES(?,?,?,?,?,?)',params)
-    if (dbResult != -1) {
-        user.setId(dbResult);
-        user.password=null;
+    const dbResult = await dbquery('add', 'INSERT INTO user (firstname, lastname, login,passwd,rankUser, email) VALUES(?,?,?,?,?,?)', params)
+    if (dbResult.error === 0) {
+        user.setId(dbResult.result);
+        user.password = null;
         return user;
     }
     else {
-        return undefined;
+        return dbResult.result;
     }
 }
 
-const updateUser=async(user)=>{
-    const {id}=user;
-    const dbResult = await dbquery('get', 'SELECT firstname,id,lastname,rankUser,login, passwd, email FROM user WHERE id=?',[id]);
-    if(dbResult.length>0){
-        const {firstname,lastname,login,passwd,rankUser,email}=dbResult[0];
-        user.update(firstname,lastname,login,passwd,rankUser,email,id);
-        const dbUpdate = await dbquery('update', 'UPDATE user SET firstname=?,lastname=?,rankUser=?,login=?,email=?,passwd=?  WHERE id=?', [
-            user.firstname,
-            user.lastname,
-            user.rank,
-            user.login,
-            user.email,
-            user.password,
-             id]);
-        return dbUpdate;
+const updateUser = async (user) => {
+    const { id } = user;
+    const dbResult = await dbquery('get', 'SELECT firstname,id,lastname,rankUser,login, passwd, email FROM user WHERE id=?', [id]);
+    if (dbResult.error=== 0) {
+        if (dbResult.result.length > 0) {
+            const { firstname, lastname, login, passwd, rankUser, email } = dbResult.result[0];
+            user.update(firstname, lastname, login, passwd, rankUser, email, id);
+            const dbUpdate = await dbquery('update', 'UPDATE user SET firstname=?,lastname=?,rankUser=?,login=?,email=?,passwd=?  WHERE id=?', [
+                user.firstname,
+                user.lastname,
+                user.rank,
+                user.login,
+                user.email,
+                user.password,
+                id]);
+            return dbUpdate;
+        }
     }
-    else{
-        return 0;
+    else {
+        return dbResult.result;
     }
 
 }
 
-const updateRank=async(id,rank)=>{
-    const dbResult = await dbquery('get', 'UPDATE  user set rankUser=? WHERE id=?',[rank,id]);
+const updateRank = async (id, rank) => {
+    const dbResult = await dbquery('get', 'UPDATE  user set rankUser=? WHERE id=?', [rank, id]);
     return dbResult;
 }
 
-const deleteUser=async(id)=>{
+const deleteUser = async (id) => {
     const dbResult = await dbquery('delete', 'DELETE FROM user WHERE id=?', [id]);
     return dbResult;
 }
 
-const setToken=async(token,id)=>{
-    const dbResult= await dbquery('update','UPDATE user SET refreshToken=? WHERE id=?',[token,id]);
+const setToken = async (token, id) => {
+    const dbResult = await dbquery('update', 'UPDATE user SET refreshToken=? WHERE id=?', [token, id]);
     return dbResult;
 }
 
-const deleteTokenDb=async(token)=>{
-    const dbResult= await dbquery('update','UPDATE user SET refreshToken=null WHERE refreshToken=?',[token]);
+const deleteTokenDb = async (token) => {
+    const dbResult = await dbquery('update', 'UPDATE user SET refreshToken=null WHERE refreshToken=?', [token]);
     return dbResult;
 }
 
 
-const findUserByToken=async(token)=>{
-    const dbResult = await dbquery('get', 'SELECT firstname,id,lastname,rankUser FROM user WHERE refreshToken=?',[token]);
-    if (dbResult && dbResult !== -1) {
-        const resultat = dbResult.map(element => {
-            const item = new User(element.firstname,element.lastname,element.login,element.passwd,element.rankUser,null,element.id);
+const findUserByToken = async (token) => {
+    const dbResult = await dbquery('get', 'SELECT firstname,id,lastname,rankUser FROM user WHERE refreshToken=?', [token]);
+    if (dbResult.error === 0) {
+        const resultat = dbResult.result.map(element => {
+            const item = new User(element.firstname, element.lastname, element.login, element.passwd, element.rankUser, null, element.id);
             return item;
         });
         return resultat;
     }
-    else if(dbResult===-1)
-    {
-        return undefined;
-    }
     else
-        return -1;
+        return dbResult.result;
 }
 
-const findCredentialsByUser=async(firsname,lastname,token)=>{
-    const dbResult = await dbquery('get', 'SELECT firstname, lastname, rankUser, id  FROM user WHERE (firstname=? and lastname=? and refreshToken=?)', [firsname,lastname,token]);
-    return dbResult[0];
-    
+const findCredentialsByUser = async (firsname, lastname, token) => {
+    const dbResult = await dbquery('get', 'SELECT firstname, lastname, rankUser, id  FROM user WHERE (firstname=? and lastname=? and refreshToken=?)', [firsname, lastname, token]);
+    if (dbResult.error === 0)
+        return dbResult.result[0];
+    return dbResult.result;
+
 }
 
-const addModelInStock=async(user,model)=>{
-    const dbResult=dbquery('add','INSERT INTO model_user (state,owner,model) VALUES (1,?,?)',[user,model]);
+const addModelInStock = async (user, model) => {
+    const dbResult = dbquery('add', 'INSERT INTO model_user (state,owner,model) VALUES (1,?,?)', [user, model]);
     if (dbResult && dbResult != -1) {
         return dbResult;
     }
@@ -152,13 +143,13 @@ const addModelInStock=async(user,model)=>{
     }
 }
 
-const getModelStockInfoById=async(id)=>{
-        const dbResult=await dbquery('get','SELECT * FROM mymodels WHERE id=?',[id]);
-        return dbResult[0];
+const getModelStockInfoById = async (id) => {
+    const dbResult = await dbquery('get', 'SELECT * FROM mymodels WHERE id=?', [id]);
+    return dbResult[0];
 }
 
-const getModelStockById=async(id,idUser)=>{
-    const dbResult=await dbquery('get','SELECT * FROM model_user WHERE owner=? AND id=?',[idUser,id]);
+const getModelStockById = async (id, idUser) => {
+    const dbResult = await dbquery('get', 'SELECT * FROM model_user WHERE owner=? AND id=?', [idUser, id]);
     if (dbResult && dbResult != -1) {
         return dbResult;
     }
@@ -168,22 +159,22 @@ const getModelStockById=async(id,idUser)=>{
     }
 }
 
-const deleteModelStock=async(id)=>{
-    const dbResult=await dbquery('delete','DELETE FROM model_user WHERE id=?',[id]);
+const deleteModelStock = async (id) => {
+    const dbResult = await dbquery('delete', 'DELETE FROM model_user WHERE id=?', [id]);
     return dbResult;
 }
 
-module.exports={
+module.exports = {
     addUser,
-    findAll,
-    findOne,
+    findAll, //OK
+    findOne, //OK
     deleteUser,
     updateUser,
-    findOneByLogin,
-    setToken,
-    deleteTokenDb,
-    findUserByToken,
-    findCredentialsByUser,
+    findOneByLogin, //OK
+    setToken, //OK
+    deleteTokenDb, //OK
+    findUserByToken, //A vérifier
+    findCredentialsByUser,  //A vérifier
     addModelInStock,
     updateRank,
     getModelStockById,
