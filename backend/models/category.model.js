d787b9edcd74709391c90ef6479353caefaff69a
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-07-02 15:53:05 +0200

    refactor(category): update models and controller to adapt to response from DB

--- backend/models/category.model.js
+++ backend/models/category.model.js
@@ -1,59 +1,58 @@
-const Category=require('../classes/Category.class');
+const Category = require('../classes/Category.class');
 const { dbquery } = require('../utils/dbutils');
 
-const findAll=async()=>{
+const findAll = async () => {
     const dbResult = await dbquery('get', 'SELECT * FROM category ORDER BY name');
-    if (dbResult && dbResult !== -1) {
-        const resultat = dbResult.map(element => {
+    if (dbResult.error === 0) {
+        const resultArray = dbResult.result;
+        const resultat = resultArray.map(element => {
             const item = new Category(element.id, element.name);
             return item;
         });
         return resultat;
     }
-    else if(dbResult===-1)
-    {
-        return undefined;
+    else {
+        return dbResult.error;
     }
-    else
-        return -1;
 }
 
-const findOne=async(id)=>{
+const findOne = async (id) => {
     const dbResult = await dbquery('get', 'SELECT * FROM category WHERE id=?', [id]);
-    if (dbResult !== -1) {
-        if (dbResult.length > 0) {
-            const category= new Category(dbResult[0].id, dbResult[0].name)
+    if (dbResult.error === 0) {
+        const resultArray = dbResult.result;
+        if (resultArray.length > 0) {
+            const category = new Category(resultArray[0].id, resultArray[0].name)
             return category;
         }
         else
-            return false;
+            return {};
     }
     else
-        return -1;
+        dbResult.error;
 }
 
-const addOne=async(category)=>{
+const addOne = async (category) => {
     const dbResult = await dbquery('add', 'INSERT INTO category (name) VALUES(?)', [category.name]);
-    if (dbResult != -1) {
-        category.setId(dbResult);
+    if (dbResult.error === 0) {
+        category.setId(dbResult.result);
         return category;
     }
     else {
-        return undefined;
+        return dbResult.result;
     }
 }
 
-const updateOne=async(category)=>{
+const updateOne = async (category) => {
     const dbResult = await dbquery('update', 'UPDATE category SET name=? WHERE id=?', [category.name, category.id]);
     return dbResult;
 }
 
-const deleteOne=async(id)=>{
+const deleteOne = async (id) => {
     const dbResult = await dbquery('delete', 'DELETE FROM category WHERE id=?', [id]);
     return dbResult;
 }
 
-module.exports={
+module.exports = {
     findAll,
     findOne,
     addOne,
