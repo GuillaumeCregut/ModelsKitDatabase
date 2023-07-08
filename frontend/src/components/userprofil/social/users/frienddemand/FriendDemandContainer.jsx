e07fa656db4693bd736d2cffe1e85ccaf42814eb
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-07-08 13:18:41 +0200

    feat(front): friendship demand are requested and displayed

--- frontend/src/components/userprofil/social/users/frienddemand/FriendDemandContainer.jsx
+++ frontend/src/components/userprofil/social/users/frienddemand/FriendDemandContainer.jsx
@@ -1,18 +1,45 @@
-import React from 'react'
-import './FriendDemandContainer.scss';
+import { useState, useEffect } from 'react';
 import FriendDemand from './FriendDemand';
-const FriendDemandContainer = ({setReload}) => {
+import useAxiosPrivate from '../../../../../hooks/useAxiosPrivate';
+
+import './FriendDemandContainer.scss';
+import { toast } from 'react-toastify';
+import { AwaitLoad } from '../../../../awaitload/AwaitLoad';
+
+const FriendDemandContainer = ({ setReload }) => {
+    const axiosPrivate = useAxiosPrivate();
+    const [demandList, setDemandList] = useState([]);
+    const [loaded, setLoaded] = useState(false);
+
+    useEffect(() => {
+        const getDemands = () => {
+            const url = `${import.meta.env.VITE_APP_API_URL}friends/demands`;
+            axiosPrivate
+                .get(url)
+                .then((resp) => {
+                    setDemandList(resp.data);
+                    setLoaded(true);
+                })
+                .catch((err) => {
+                    toast.error('Une erreur est survenue');
+                })
+        }
+        getDemands();
+    }, []);
+
     return (
         <div className='friend-demand-container'>
             <h3 className='demand-title'>Demandes d'amis reçues</h3>
             <div className="friend-demand-card-container">
-                <FriendDemand setReload={setReload}/>
-                <FriendDemand setReload={setReload}/>
-                <FriendDemand setReload={setReload}/>
-                <FriendDemand setReload={setReload}/>
-                <FriendDemand setReload={setReload}/>
-                <FriendDemand setReload={setReload}/>
-                <FriendDemand setReload={setReload}/>
+                {
+                    loaded
+                        ? (demandList.map((demand) => (
+                            <FriendDemand setReload={setReload} key={demand.id} user={demand} />
+                        ))
+                        )
+                        : <AwaitLoad />
+                }
+
             </div>
         </div>
     )
