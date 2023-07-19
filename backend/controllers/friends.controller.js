c2fa8868e7b5f6d00543da0bf3c2c823c7d74747
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-07-19 11:55:03 +0200

    fix(back): error when no images, return full list of files. Corrected

--- backend/controllers/friends.controller.js
+++ backend/controllers/friends.controller.js
@@ -39,8 +39,8 @@ const getAllVisible = async (req, res) => {
 }
 
 const unlinkUser = async (req, res) => {
-    const deleteLink=await friendsModel.updateFriendship(req.friendId, req.user.user_id,friendState.unknonwn);
-    if(deleteLink.error===0){
+    const deleteLink = await friendsModel.updateFriendship(req.friendId, req.user.user_id, friendState.unknonwn);
+    if (deleteLink.error === 0) {
         return res.sendStatus(204);
     }
     return res.sendStatus(500);
@@ -66,45 +66,45 @@ const changeDemand = async (req, res) => {
     if (!(Object.values(friendState).indexOf(statusFriend) > -1)) {
         return res.sendStatus(422);
     }
-    const upateResult=await friendsModel.updateFriendship(idUser,friendId,statusFriend);
-    if(upateResult.error===0){
-        if(upateResult.result)
-            return  res.sendStatus(204);
+    const upateResult = await friendsModel.updateFriendship(idUser, friendId, statusFriend);
+    if (upateResult.error === 0) {
+        if (upateResult.result)
+            return res.sendStatus(204);
         return res.sendStatus(404);
     }
     return res.sendStatus(500);
 }
 
-const addFriendShip=async(req,res)=>{
-    const {friendId}=req.body;
-    if(!friendId)
+const addFriendShip = async (req, res) => {
+    const { friendId } = req.body;
+    if (!friendId)
         return res.sendStatus(422);
-    
-    const addRelation=await friendsModel.addFriendShip( req.user.user_id,friendId,friendState.waiting);
-    if(addRelation.error===0){
+
+    const addRelation = await friendsModel.addFriendShip(req.user.user_id, friendId, friendState.waiting);
+    if (addRelation.error === 0) {
         return res.sendStatus(201);
     }
-    switch(addRelation.result){
-        case 1062 : return res.sendStatus(409);
-        case 1452 : return res.sendStatus(404);
-        default : return res.sendStatus(500);
+    switch (addRelation.result) {
+        case 1062: return res.sendStatus(409);
+        case 1452: return res.sendStatus(404);
+        default: return res.sendStatus(500);
     }
 
 }
 
-const getFriends=async(req,res)=>{
-    const allFriends=await friendsModel.getFriends(req.user.user_id,friendState.friend);
-    if(allFriends.error===0){
+const getFriends = async (req, res) => {
+    const allFriends = await friendsModel.getFriends(req.user.user_id, friendState.friend);
+    if (allFriends.error === 0) {
         //Get messages count
-        const getMessages=await friendsModel.getMessageCount(req.user.user_id);
-        if (getMessages.error===0){
-            const listMessage=getMessages.result;
-            const fullList=allFriends.result.map((friend)=>{
-                const messages=listMessage.find(elt=>elt.exp===friend.id);
-                let nbMessage=0
-                if(messages)
-                    nbMessage=messages.nb;
-                return {...friend,nbMessage}
+        const getMessages = await friendsModel.getMessageCount(req.user.user_id);
+        if (getMessages.error === 0) {
+            const listMessage = getMessages.result;
+            const fullList = allFriends.result.map((friend) => {
+                const messages = listMessage.find(elt => elt.exp === friend.id);
+                let nbMessage = 0
+                if (messages)
+                    nbMessage = messages.nb;
+                return { ...friend, nbMessage }
             })
             return res.json(fullList);
         }
@@ -114,33 +114,30 @@ const getFriends=async(req,res)=>{
         return res.sendStatus(500);
 }
 
-const getFriendModels=async(req,res)=>{
-    const friendId=req.params.id;
-    const friendsModels=await friendsModel.getFriendModels(friendId);
-    if(friendsModels.error===0){
+const getFriendModels = async (req, res) => {
+    const friendId = req.params.id;
+    const friendsModels = await friendsModel.getFriendModels(friendId);
+    if (friendsModels.error === 0) {
         return res.json(friendsModels.result);
     }
     return res.sendStatus(500);
-    
+
 }
 
-const getFriendModelDetails=async(req, res)=>{
-    if(isNaN(req.params.idModel))
+const getFriendModelDetails = async (req, res) => {
+    if (isNaN(req.params.idModel))
         return res.sendStatus(422);
-    const idModel=parseInt(req.params.idModel, 10);
-    const friendId=req.params.id;
-    const detailsModel=await friendsModel.getFriendModelDetails(idModel,friendId);
+    const idModel = parseInt(req.params.idModel, 10);
+    const friendId = req.params.id;
+    const detailsModel = await friendsModel.getFriendModelDetails(idModel, friendId);
     const fileArray = [];
-    if(detailsModel.error===0){
-        if(detailsModel.result.length===0)
+    if (detailsModel.error === 0) {
+        if (detailsModel.result.length === 0)
             return res.sendStatus(404);
-        
-        if(detailsModel.result[0].pictures!==null){
-            
-         const basePath = detailsModel.result[0].pictures;
-         
-         const pathModel = path.join(__dirname, '..', basePath);
-        await fs.promises.readdir(pathModel)
+        if (detailsModel.result[0].pictures !== null && detailsModel.result[0].pictures !== '') {
+            const basePath = detailsModel.result[0].pictures;
+            const pathModel = path.join(__dirname, '..', basePath);
+            await fs.promises.readdir(pathModel)
                 .then(filenames => {
                     for (let filename of filenames) {
                         fileArray.push(filename)
@@ -152,10 +149,10 @@ const getFriendModelDetails=async(req, res)=>{
                 })
         }
         //Get messages and add them to object
-        const messages=await friendsModel.getModelMessage(idModel);
-        if(messages.error===0){
-            const details={...detailsModel.result[0],fileArray,messages:messages.result};
-            return res.json(details);    
+        const messages = await friendsModel.getModelMessage(idModel);
+        if (messages.error === 0) {
+            const details = { ...detailsModel.result[0], fileArray, messages: messages.result };
+            return res.json(details);
         }
         return res.sendStatus(500);
     }
