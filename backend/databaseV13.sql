2cc1898c192dc7d4baea01f331ac71a25ad75954
Guillaume Crégut <gcregut@free.fr>
2023-10-15 10:08:46 +0200

    refactor(database): change view name

--- backend/databaseV13.sql
+++ backend/databaseV13.sql
@@ -1,6 +1,6 @@
 ALTER TABLE `user` ADD `isvalid` BOOLEAN NOT NULL AFTER `allow`;
 
-CREATE VIEW `model_full2`
+CREATE VIEW `model_fullwithCountry`
  AS select mu.id AS id,mu.model AS model,mu.provider AS provider,mu.pictures AS pictures,mu.price AS price,mu.owner AS owner,
 mu.state AS state,m.name AS modelName,m.picture AS picture,m.reference AS reference,m.scalemates AS scalemates,b.name AS brandName,
 p.name AS periodName,s.name AS scaleName,bu.name AS builderName,c.name AS categoryName,pr.name AS providerName,co.name as countryName,
